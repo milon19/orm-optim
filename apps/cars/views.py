2d65c32337cb757a4197551aa403eafcacf7a120
milon19 <milonmahato67@gmail.com>
2025-02-05 22:33:33 +0600

    feat: install django debug tool bar and add pagination in api

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 
 from rest_framework.response import Response
@@ -8,11 +9,14 @@ from apps.cars.serializers import CarListSerializer
 
 class CarListAPIView(APIView):
     serializer_class = CarListSerializer
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         return Car.objects.all()
 
     def get(self, request):
-        queryset = self.get_queryset()
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
+        qs = self.get_queryset()
+        paginator = self.pagination_class()
+        paginated_queryset = paginator.paginate_queryset(qs, request, self)
+        serializer = self.serializer_class(paginated_queryset, many=True)
+        return paginator.get_paginated_response(serializer.data)
--- config/settings.py
+++ config/settings.py
@@ -19,9 +19,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'apps.core',
     'apps.cars',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -96,4 +98,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
